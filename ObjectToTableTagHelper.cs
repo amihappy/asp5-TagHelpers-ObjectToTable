960db56a6036038bb83dc1e0b158f59a812f765e
Shather <arthas29@gmail.com>
2016-02-01 21:31:38 +0100

    alpha

---
+++ ObjectToTableTagHelper.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Security.AccessControl;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc.Rendering;
+using Microsoft.AspNet.Mvc.ViewFeatures;
+using Microsoft.AspNet.Razor.TagHelpers;
+using Newtonsoft.Json;
+
+namespace ObjectToTableHelper
+{
+    [HtmlTargetElement("object-to-table", TagStructure = TagStructure.Unspecified)]
+    public class ObjectToTableTagHelper : TagHelper
+    {
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "table";
+            var c = await output.GetChildContentAsync();
+            var input = c.GetContent().Replace("&quot;", "\"");
+            Regex r = new Regex("({|,)\\D(?<propName>\\w+)\\D:\\D(?<value>[^\"]+)\\D");
+            var matches = r.Matches(input);
+            var outputBuilder = new StringBuilder();
+            foreach (Match match in matches)
+            {
+                var prop = match.Groups["propName"];
+                var value = match.Groups["value"];
+                outputBuilder.Append($"<tr><td>{prop.Value}</td><td>{value.Value}</td><tr>");
+            }
+
+            output.Content.SetContent(new HtmlString(outputBuilder.ToString()));
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # asp5-TagHelpers-ObjectToTable
 simple tag helper to display json object in table using new taghelpers.
+
+usage:
+```
+<object-to-table>@Model</object-to-table>
+```
